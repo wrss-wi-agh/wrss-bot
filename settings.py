ad299b5cbe83db4b086fc924da4d7632db82ea85
bartosz rzepa <bartosz.rzepa@swmansion.com>
2024-08-04 19:03:28 +0200

    corrected seen emoji to accept unicode characters

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 seen_emoji_long_id = os.getenv("SEEN_EMOJI_LONG_ID")
 doodle_channel_id = int(os.getenv("DOODLE_CHANNEL_ID"))
-doodle_seen_reaction = os.getenv("DOODLE_SEEN_REACTION")
+doodle_seen_reaction = chr(int(os.getenv("DOODLE_SEEN_REACTION"), 0))
 notify_role_id = int(os.getenv("NOTIFY_ROLE_ID"))
 client_token = os.getenv("DISCORD_CLIENT_TOKEN")
 doodle_links = os.getenv("DOODLE_LINKS").split(',')
